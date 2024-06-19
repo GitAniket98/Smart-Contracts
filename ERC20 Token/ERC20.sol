88dcc863d88366ab2ef354fb0989b11ced00924b
Aniket Chaudhary <aniketchaudhary9838@gmail.com>
2024-06-19 13:38:28 +0530

    Update ERC20.sol

--- ERC20 Token/ERC20.sol
+++ ERC20 Token/ERC20.sol
@@ -1,1 +1,76 @@
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8;
+
+import "./IERC20.sol";
+
+contract ERC20 is IERC20 {
+
+   
+    
+    uint256 public totalSupply;
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
+    string public name = "CoffeeToken";
+    string public symbol = "COFFEE";
+    uint8 public decimals = 18;
+
+
+
+    constructor(string memory _name, string memory _symbol, uint8 _decimals){
+        name = _name;
+        symbol = _symbol;
+        decimals = _decimals;
+    }
+
+    function transfer(address recipient, uint256 amount) external returns(bool){
+        balanceOf[msg.sender] -= amount;
+        balanceOf[recipient]  += amount;
+        emit Transfer(msg.sender, recipient, amount);
+        return true;
+      }
+
+
+    function approve(address spender, uint256 amount) external returns (bool){
+        allowance[msg.sender][spender] = amount;
+        emit Approval(msg.sender, spender, amount);
+        return true;
+    }
+
+
+    function transferFrom(address sender, address recipient, uint256 amount) 
+    external returns(bool){
+        allowance[sender][msg.sender] -= amount;
+        balanceOf[sender] -= amount;
+        balanceOf[recipient] += amount;
+        emit Transfer(sender, recipient, amount);
+        return true;
+
+    }
+
+    function _mint(address _to, uint256 _amount) internal {
+        balanceOf[_to] += _amount;
+        totalSupply += _amount;
+        emit Transfer(address(0), _to, _amount);
+    }
+
+    function _burn(address _from, uint256 _amount) internal {
+        balanceOf[_from] -= _amount;
+        totalSupply -= _amount;
+        emit Transfer(_from, address(0), _amount);
+
+    }
+
+    function mint(address _to, uint256  _amount) external {
+        _mint(_to, _amount);
+    }
+    
+
+    function burn(address _from, uint256 _amount) external {
+        _burn(_from, _amount);
+    }
+
+
+
+
+}
