e6c28f91caf05471855ee13430a2c53bdc7b8c3e
GitAniket98 <aniketchaudhary9838@gmail.com>
2024-06-19 14:16:45 +0530

    Added comment

--- ERC20 Token/ERC20.sol
+++ ERC20 Token/ERC20.sol
@@ -1,13 +1,9 @@
-
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8;
-
+//we are importing IERC20 interface
 import "./IERC20.sol";
 
 contract ERC20 is IERC20 {
-
-   
-    
     uint256 public totalSupply;
     mapping(address => uint256) public balanceOf;
     mapping(address => mapping(address => uint256)) public allowance;
@@ -15,37 +11,38 @@ contract ERC20 is IERC20 {
     string public symbol = "COFFEE";
     uint8 public decimals = 18;
 
-
-
-    constructor(string memory _name, string memory _symbol, uint8 _decimals){
+    constructor(string memory _name, string memory _symbol, uint8 _decimals) {
         name = _name;
         symbol = _symbol;
         decimals = _decimals;
     }
 
-    function transfer(address recipient, uint256 amount) external returns(bool){
+    function transfer(
+        address recipient,
+        uint256 amount
+    ) external returns (bool) {
         balanceOf[msg.sender] -= amount;
-        balanceOf[recipient]  += amount;
+        balanceOf[recipient] += amount;
         emit Transfer(msg.sender, recipient, amount);
         return true;
-      }
+    }
 
-
-    function approve(address spender, uint256 amount) external returns (bool){
+    function approve(address spender, uint256 amount) external returns (bool) {
         allowance[msg.sender][spender] = amount;
         emit Approval(msg.sender, spender, amount);
         return true;
     }
 
-
-    function transferFrom(address sender, address recipient, uint256 amount) 
-    external returns(bool){
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) external returns (bool) {
         allowance[sender][msg.sender] -= amount;
         balanceOf[sender] -= amount;
         balanceOf[recipient] += amount;
         emit Transfer(sender, recipient, amount);
         return true;
-
     }
 
     function _mint(address _to, uint256 _amount) internal {
@@ -58,19 +55,13 @@ contract ERC20 is IERC20 {
         balanceOf[_from] -= _amount;
         totalSupply -= _amount;
         emit Transfer(_from, address(0), _amount);
-
     }
 
-    function mint(address _to, uint256  _amount) external {
+    function mint(address _to, uint256 _amount) external {
         _mint(_to, _amount);
     }
-    
 
     function burn(address _from, uint256 _amount) external {
         _burn(_from, _amount);
     }
-
-
-
-
 }
