71be997ed522f2ce5fdbf1fa85d0dde8d13b2cf9
GitAniket98 <aniketchaudhary9838@gmail.com>
2024-06-21 13:59:00 +0530

    added comment

--- Multi Signature Wallet/MultiSig.sol
+++ Multi Signature Wallet/MultiSig.sol
@@ -24,6 +24,7 @@ contract MultiSig {
     mapping(uint => mapping(address => bool)) public approved;
 
     uint public required;
+    // Modifier to check for onlyOwner
 
     modifier onlyOwner() {
         require(isOwner[msg.sender], "Not owner");
